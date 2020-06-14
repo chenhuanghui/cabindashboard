7ec856c1889db95ed806c518d8d65744bc41a68c
enZo <inbox.huytran@gmail.com>
2020-06-14 14:22:26 +0700

    remove other libs

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,9 @@
 import '../public/assets/css/theme.min.css'
 
 import '../public/assets/fonts/feather/feather.css'
-import '../public/assets/libs/flatpickr/dist/flatpickr.min.css'
-import '../public/assets/libs/quill/dist/quill.core.css'
-import '../public/assets/libs/highlightjs/styles/vs2015.css'
+// import '../public/assets/libs/flatpickr/dist/flatpickr.min.css'
+// import '../public/assets/libs/quill/dist/quill.core.css'
+// import '../public/assets/libs/highlightjs/styles/vs2015.css'
 
 export default function App({ Component, pageProps }) {
     return <Component {...pageProps} />
