12d7c097b5949f5d135ac937274333b3eaa23e3d
enZo <inbox.huytran@gmail.com>
2020-06-17 00:30:24 +0700

    chart style

--- components/card/card_chart.js
+++ components/card/card_chart.js
@@ -15,7 +15,26 @@ export default class CardChart extends React.Component {
       labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
       datasets: [{
         label: 'Số điện',
-        data: [0, 10, 5, 15, 10, 20, 15, 25, 20, 30, 25, 40]
+        data: [0, 10, 5, 15, 10, 20, 15, 25, 20, 30, 25, 40],
+
+        fill: false,
+        // lineTension: 0.1,
+        backgroundColor: '#2C7BE5',
+        borderColor: '#2C7BE5',
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: '#2C7BE5',
+        pointBackgroundColor: '#fff',
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: '#2C7BE5',
+        pointHoverBorderColor: '#2C7BE5',
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+
       }]
     }
     this.setState({data:temp});
@@ -53,3 +72,27 @@ export default class CardChart extends React.Component {
 }
 
 // https://www.npmjs.com/package/react-chartjs-2?activeTab=readme
+/*
+datasets: [
+  {
+    label: 'My First dataset',
+    fill: false,
+    lineTension: 0.1,
+    backgroundColor: 'rgba(75,192,192,0.4)',
+    borderColor: 'rgba(75,192,192,1)',
+    borderCapStyle: 'butt',
+    borderDash: [],
+    borderDashOffset: 0.0,
+    borderJoinStyle: 'miter',
+    pointBorderColor: 'rgba(75,192,192,1)',
+    pointBackgroundColor: '#fff',
+    pointBorderWidth: 1,
+    pointHoverRadius: 5,
+    pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+    pointHoverBorderColor: 'rgba(220,220,220,1)',
+    pointHoverBorderWidth: 2,
+    pointRadius: 1,
+    pointHitRadius: 10,
+    data: [65, 59, 80, 81, 56, 55, 40]
+  }
+  */
