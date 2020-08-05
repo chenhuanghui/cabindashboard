f7b6144be836aea565d97aaf1b290abfb1e182b7
enZo <inbox.huytran@gmail.com>
2020-08-05 09:03:51 +0700

    update. create1

--- layouts/LayoutBrandCreateStep1.js
+++ layouts/LayoutBrandCreateStep1.js
@@ -62,40 +62,45 @@ export default class LayoutBrandCreateStep1 extends React.Component {
                                             <p className="mb-5 text-muted">Cung cấp các thông tin về thương hiệu của bạn để đăng ký với các đối tác bán hàng.</p>
                                         </div>
                                     </div>
-                                    <div className="form-group">
-                                        <label>Tên nhãn hiệu</label>
-                                        <small className="form-text text-muted">Tên trên GPKD (nếu có GPKD là Công ty/Hộ kinh doanh) thì điền chính xác tên in trên GPKD, nếu không GPKD thì ghi tên chính xác trên biển hiệu</small>
-                                        <input type="text" className="form-control"/>
-                                    </div>
-                                    <div className="form-group">
-                                        <label className="mb-1">Mô tả</label>
-                                        <small className="form-text text-muted">Giới thiệu chi tiết về thương hiệu của bạn giúp khách hàng hiểu bạn đang bán món ăn thức uống nào, có phù hợp với nhu cầu của khách hàng không</small>
-                                        <textarea class="form-control" data-toggle="autosize" rows="1" placeholder="Không quá 80 từ..."></textarea>
-                                    </div>
-                                    <div className="form-group">
-                                        <label>Mã số doanh nghiệp</label>
-                                        <small className="form-text text-muted"> VD : nếu là HKD 41A... , 41X... - nếu là công ty : 0311..... ) Không có GPKD điền " Không có "</small>
-                                        <input type="text" className="form-control"/>
-                                    </div>
-                                    <div className="form-group">
-                                        <label>Ngày đăng ký kinh doanh</label>
-                                        <small className="form-text text-muted">Điền chính xác trên GPKD nếu có - không có ghi " Không có </small>
-                                        <input type="text" className="form-control"/>
+                                    <div className='card'>
+                                        <div className='card-body'>
+                                            <div className="form-group">
+                                                <label>Tên nhãn hiệu</label>
+                                                <small className="form-text text-muted">Tên trên GPKD (nếu có GPKD là Công ty/Hộ kinh doanh) thì điền chính xác tên in trên GPKD, nếu không GPKD thì ghi tên chính xác trên biển hiệu</small>
+                                                <input type="text" className="form-control"/>
+                                            </div>
+                                            <div className="form-group">
+                                                <label className="mb-1">Mô tả</label>
+                                                <small className="form-text text-muted">Giới thiệu chi tiết về thương hiệu của bạn giúp khách hàng hiểu bạn đang bán món ăn thức uống nào, có phù hợp với nhu cầu của khách hàng không</small>
+                                                <textarea className="form-control" data-toggle="autosize" rows="1" placeholder="Không quá 80 từ..."></textarea>
+                                            </div>
+                                            <div className="form-group">
+                                                <label>Mã số doanh nghiệp</label>
+                                                <small className="form-text text-muted"> VD : nếu là HKD 41A... , 41X... - nếu là công ty : 0311..... ) Không có GPKD điền " Không có "</small>
+                                                <input type="text" className="form-control"/>
+                                            </div>
+                                            <div className="form-group">
+                                                <label>Ngày đăng ký kinh doanh</label>
+                                                <small className="form-text text-muted">Điền chính xác trên GPKD nếu có - không có ghi " Không có </small>
+                                                <input type="text" className="form-control"/>
+                                            </div>
+                                            
+                                            <hr className="my-5" />
+
+                                            <div className="row align-items-center">
+                                                <div className="col-auto">
+                                                    <button className="btn btn-lg btn-white" type="reset">Cancel</button>
+                                                </div>
+                                                <div className="col text-center">
+                                                    <h6 className="text-uppercase text-muted mb-0">Step 1 of 3</h6>
+                                                </div>
+                                                <div className="col-auto">
+                                                    <a className="btn btn-lg btn-primary" data-toggle="wizard" href="#wizardStepTwo">Continue</a>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </div>
                                     
-                                    <hr className="my-5" />
-
-                                    <div className="row align-items-center">
-                                        <div className="col-auto">
-                                            <button className="btn btn-lg btn-white" type="reset">Cancel</button>
-                                        </div>
-                                        <div className="col text-center">
-                                            <h6 className="text-uppercase text-muted mb-0">Step 1 of 3</h6>
-                                        </div>
-                                        <div className="col-auto">
-                                            <a className="btn btn-lg btn-primary" data-toggle="wizard" href="#wizardStepTwo">Continue</a>
-                                        </div>
-                                    </div>
                                 </div>
                             </div>                                                                                      
                         </div>
