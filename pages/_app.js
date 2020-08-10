a9ed94e9e6750841ac41201cf833e725add06bb2
enZo <inbox.huytran@gmail.com>
2020-08-10 17:58:36 +0700

    link css all in _document

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@
 // import '../public/assets/css/style.css'
 // import '../public/assets/fonts/feather/feather.css'
 // import '../public/assets/libs/flatpickr/dist/flatpickr.min.css'
-import '../public/assets/css/flatpickr.min.css'
+// import '../public/assets/css/flatpickr.min.css'
 // import '../public/assets/libs/quill/dist/quill.core.css'
 // import '../public/assets/libs/highlightjs/styles/vs2015.css'
 // import $ from 'jquery';
